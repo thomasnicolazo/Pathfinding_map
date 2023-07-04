7f430eedfe2aaa32bb21f478aa3c19bb8a52bb5c
Thomas Nicolazo <thomas.nicolazo@gmail.com>
2023-07-04 12:18:23 +0200

    add comment to Map.hpp ,to the methods of the Map class

--- Map.hpp
+++ Map.hpp
@@ -21,24 +21,24 @@ public:
 
     int getMapHeight();
 
-    void changeColor(int x, int y,std::string couleur);
+    void changeColor(int x, int y,std::string couleur); // change element color in map
 
     void resetColor(std::string couleur);
 
     void displayMap();
 
-    Terrain getElementAt(int x, int y);
+    Terrain getElementAt(int x, int y); // get element terrain at  specific coordinates
 
-    void setElement(int x, int y , Terrain value);
+    void setElement(int x, int y , Terrain value); // set element terrain at specific coordinates
 
-    void addTop(int x,int y,Terrain value);
+    void addTop(int x,int y,Terrain value); // add lines on the top of the map with terrain
 
-    void addRight(int x, int y, Terrain value);
+    void addRight(int x, int y, Terrain value); // add columns on the right of the map with terrain
 
-    void addBottom(int x, int y ,Terrain value);
+    void addBottom(int x, int y ,Terrain value); // add lines on the bottom of the map with terrain 
 
-    void addLeft(int x, int y,Terrain value);
+    void addLeft(int x, int y,Terrain value); // add columns on the left of the map with terrain
 
-    void addElement(int x, int y, Terrain value);
+    void addElement(int x, int y, Terrain value); // fill the difference between the point(with coordinates x,y) and the map with terrain
 };
 
